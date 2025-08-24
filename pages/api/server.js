fee286032a44aa9623d85ed022648ce8cb04e378
Quique <frameloop@gmail.com>
2025-08-24 23:35:59 +0200

    Configuración simple para Vercel - sin Next.js

--- pages/api/server.js
+++
@@ -1,5 +0,0 @@
-// Este archivo redirige todas las peticiones a tu servidor personalizado
-export default function handler(req, res) {
-    // Redirigir a tu servidor personalizado
-    res.redirect(307, '/');
-}
